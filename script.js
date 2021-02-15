f3eec6092d052db99c6ce6bd2dad8c3947d9eefa
janne-a <50884316+janne-a@users.noreply.github.com>
2021-02-15 13:37:48 +0200

    feat: logs random characters

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-console.log('this is test')
-console.log('line 2')
-console.log('line 3')
-console.log('line 4')
-console.log('line 5')
-console.log('this is ')
+// create arrays to hold text strings
+
+const stSel1 = ["1.1", "1.2", "1.3"]
+const stSel2 = ["2.1", "2.2", "2.3"]
+const stSel3 = ["3.1", "3.2", "3.3"]
+
+// take one random string from each and put them together
+const randomNumber = () => Math.floor(Math.random() * 3 )
+console.log(stSel1[randomNumber()] + ' ' + stSel2[randomNumber()] + ' ' + stSel3[randomNumber()] )                                                           
+ 
+
+
